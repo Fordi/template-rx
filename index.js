6382256dc416f86544dad9f1141c684f866947ec
Bryan Elliott <fordiman@gmail.com>
2019-04-20 11:41:58 -0400

    Small performance improvement.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const rx = (strings, ...expressions) => {
-  let flags = '';
-  let expr = strings.map((string, index) => string + (
+  const expr = strings.map((string, index) => string + (
     Array.isArray(expressions[index])
       ? `(?:${expressions[index].map(rx.escape).join('|')})`
       : rx.escape(expressions[index])
   )).join('');
-  const m = expr.match(/\/([gimsuy]*)$/);
-  if (m && expr[0] === '/') {
-    expr = expr.substr(1, expr.length - 1 - m[0].length);
-    flags = m[1];
+  if (expr[0] === '/') {
+    const m = expr.match(/\/([gimsuy]*)$/);
+    if (m) {
+      return new RegExp(expr.substr(1, expr.length - 1 - m[0].length), m[1]);
+    }
   }
-  return new RegExp(expr, flags);
+  return new RegExp(expr);
 };
 
 rx.escape = src => {
@@ -18,16 +18,3 @@ rx.escape = src => {
 };
 
 module.exports = rx;
-
-
-const selfCloser = tagName => rx`/<(${tagName})([^>]+)/>/ig`;
-
-const html = '<html><head><meta charset="utf-8" /></head><body><img src="logo.png" /></html>';
-// /<meta([^>]+)\/>/g
-const metaRx = selfCloser('meta');
-// /<(?:meta|img)([^>]+)\/>/g
-const metaImgRx = selfCloser(['meta', 'img']);
-
-console.log(html.match(metaRx));
-// > ['<meta charset="utf-8" />', ' charset="utf-8" ']
-console.log(html.match(metaImgRx));
